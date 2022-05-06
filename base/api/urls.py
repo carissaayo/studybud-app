597e2853758cebbae626c964f6568a6c8f2fbf81
yussuf ajao <ajaoyussufayo@gmail.com>
2022-05-06 08:05:07 +0100

    initial commit

---
+++ base/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("",views.getRoutes),
+    path("rooms/", views.getRooms, name=""),
+    path("rooms/<str:pk>/", views.getRoom, name="")
+]
